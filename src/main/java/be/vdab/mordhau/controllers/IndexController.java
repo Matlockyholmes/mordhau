d95a9b43637a319b4ca2b7fda7cb90006562338a
Matlockyholmes <matthiasheylen1@gmail.com>
2019-05-16 17:58:57 +0200

    Help

--- src/main/java/be/vdab/mordhau/controllers/IndexController.java
+++ src/main/java/be/vdab/mordhau/controllers/IndexController.java
@@ -1,45 +1,40 @@
 package be.vdab.mordhau.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalTime;
 import java.util.Calendar;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class IndexController {
     @GetMapping
-    public String mordhauPageMaker(){
-        return new StringBuffer("<!DOCTYPE html>\n" +
-                "<html lang=\"en\">\n" +
-                "<head>\n" +
-                "    <meta charset=\"UTF-8\">\n" +
-                "    <title>MordhauProject</title>\n" +
-                "    <link rel=\"icon\" href=\"icons/mordhauico.ico\" type=\"image/x-icon\">\n" +
-                "    <link rel=\"stylesheet\" href=\"css/mordhau.css\">\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "<h1>Mordhau Springbawt project</h1>\n" +
-                "<img src=\"images/indexhau.jpg\" alt=\"index pic\" class=\"fullwidth\">\n")
-        .append(welcomeManagement())
-        .append(openOrClosedValidator())
-        .append("</body>\n</html>")
-        .toString();
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView("index","welcome",hourChecker());
+        modelAndView.addObject("activity",prayingOrSlaughtering());
+        return modelAndView;
     }
-    public String welcomeManagement(){
-        int currentHour = LocalTime.now().getHour();
-        if(currentHour < 12){
-            return("<h2>Good Morning</h2>\n");
-        } else if(currentHour < 18){
-            return("<h2>Good Afternoon</h2>\n");
+
+    private String hourChecker(){
+        int hour = LocalTime.now().getHour();
+        if (hour < 12){
+           return  "morning";
+        } else if (hour < 18){
+            return "Afternoon";
         } else {
-            return("<h2>Good Night</h2>\n");
+            return "evening";
         }
     }
-    public String openOrClosedValidator(){
-        int currentDay = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
-        return currentDay == 2 || currentDay == 5 ? "<h3>No slaughter on mondays & thursdays</h3>\n" : "<h3>Time for slaughter</h3>\n";
+
+    private String prayingOrSlaughtering(){
+        int day = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
+        if (day == 2 || day == 5){
+            return "praying";
+        } else {
+            return "slaughtering";
+        }
     }
 }
