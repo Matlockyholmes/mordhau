74fe1694aa0b8d75fb2c4310e87b5178191693a5
Matlockyholmes <matthiasheylen1@gmail.com>
2019-05-16 22:03:01 +0200

    Help

--- src/main/java/be/vdab/mordhau/controllers/SwordController.java
+++ src/main/java/be/vdab/mordhau/controllers/SwordController.java
@@ -3,22 +3,32 @@ package be.vdab.mordhau.controllers;
 import be.vdab.mordhau.Domain.Sword;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 @Controller
 @RequestMapping("swords")
 public class SwordController {
     private Sword[] swordArray = {
-            new Sword(1,"Short sword", BigDecimal.valueOf(7.50),false),
-            new Sword(2,"Greatsword",BigDecimal.valueOf(18.30),true),
-            new Sword(3,"Arming sword",BigDecimal.valueOf(12.10),false),
-            new Sword(4,"Zweihander",BigDecimal.valueOf(27.10),true)
+            new Sword(1,"shortsword", BigDecimal.valueOf(7.50),false),
+            new Sword(2,"greatsword",BigDecimal.valueOf(18.30),true),
+            new Sword(3,"armingsword",BigDecimal.valueOf(12.10),false),
+            new Sword(4,"zweihander",BigDecimal.valueOf(27.10),true)
     };
     @GetMapping
     public ModelAndView swordBringer(){
         return new ModelAndView("swords","swords",swordArray);
     }
+    @GetMapping("{id}")
+    public ModelAndView detailBringer(@PathVariable long id){
+        ModelAndView modelAndView = new ModelAndView("sword");
+        Arrays.stream(swordArray).filter(sword -> sword.getId() == id).findFirst().
+                ifPresent(sword -> modelAndView.addObject("sword",sword));
+        return modelAndView;
+    }
+
 }
